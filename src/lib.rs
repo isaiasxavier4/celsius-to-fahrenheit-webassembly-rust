e5cf5edc129a50eda8896bcf59cb3ed9ea6377da
Isaias Xavier <work.isaiasxavier@gmail.com>
2025-06-22 09:40:53 -0400

    Atualização de Comentários do Projeto!

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
 //Usa a bibioteca WASM-PACK
+//O wasm-pack não é uma biblioteca em si, mas sim uma ferramentea de
+//linha de comando que auxilia no processo de criação, compilação e empacotamento
+// de código Rust para WebAssembly(Wasm) - Font Google via Gemini.
+
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
